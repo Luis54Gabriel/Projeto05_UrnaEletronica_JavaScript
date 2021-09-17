12fe35cc63635bd7435fe0df12e18b28f781af28
Luís Gabriel da Silva <89105365+Luis54Gabriel@users.noreply.github.com>
2021-09-16 21:18:23 -0300

    Add files via upload

---
+++ Projeto05_UrnaEletronica_JavaScript/etapas.js
@@ -0,0 +1,61 @@
+let etapas = [{
+        titulo: 'VEREADOR',
+        numeros: 5,
+        candidatos: [{
+                numero: '11111',
+                nome: 'Fulano de Tal',
+                partido: 'ABC',
+                fotos: [{
+                    url: 'https://randomuser.me/api/portraits/men/2.jpg',
+                    legenda: 'Vereador'
+                }]
+            },
+            {
+                numero: '22222',
+                nome: 'Beltrano de Tal',
+                partido: 'DEF',
+                fotos: [{
+                    url: 'https://randomuser.me/api/portraits/men/3.jpg',
+                    legenda: 'Vereador'
+                }]
+            },
+        ]
+    },
+    {
+        titulo: 'PREFEITO',
+        numeros: 2,
+        candidatos: [{
+                numero: '11',
+                nome: 'Ciclano de Tal',
+                partido: 'ABC',
+                vice: 'Cic',
+                fotos: [{
+                        url: 'https://randomuser.me/api/portraits/men/6.jpg',
+                        legenda: 'Prefeito'
+                    },
+                    {
+                        url: 'https://randomuser.me/api/portraits/men/5.jpg',
+                        legenda: 'Vice-Prefeito',
+                        small: true
+                    }
+                ]
+            },
+            {
+                numero: '22',
+                nome: 'Zulano de Tal',
+                partido: 'DEF',
+                vice: 'Zul',
+                fotos: [{
+                        url: 'https://randomuser.me/api/portraits/men/7.jpg',
+                        legenda: 'Prefeito'
+                    },
+                    {
+                        url: 'https://randomuser.me/api/portraits/men/8.jpg',
+                        legenda: 'Vice-Prefeito',
+                        small: true
+                    }
+                ]
+            },
+        ]
+    }
+];
